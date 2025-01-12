57c2322ffc0fa323155dd61f99a97982b5fa012a
kavita <desulukavita@gmail.com>
2025-01-12 08:04:50 -0800

    project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+1) created react app using (npm create vite@latest netflixgpt)
+2) Installed sass
+3) Installed react-router-dom
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World kavita</h1>
+    </>
+  )
+}
+
+export default App
